32627cb30f1f237bcf938d212c88ba9f77437c32
yangliouliyv <2271067531@qq.com>
2025-04-05 02:01:59 +0000

    更新 更新
    Signed-off-by: yangliouliyv <2271067531@qq.com>

---
+++ dcj/manager插件管理器.sh
@@ -0,0 +1,8 @@
+#绘安扩展安装包
+#该安装包由bilibili：杨柳鲤余 制作
+#安装包放在ubuntu目录下的/home/sd/dcj
+#必填
+#此为默认源（github）
+https="https://github.moeyy.xyz/https://github.com/ltdrdata/ComfyUI-Manager.git"
+name=ComfyUI-Manager
+#mane是插件文件夹名字，通常加速源是一样的
